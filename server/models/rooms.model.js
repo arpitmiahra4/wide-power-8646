82886687f15c6a791d9a796de6f2625602bbcd8f
HARSHAL-AP <apsundeharshal129@gmail.com>
2023-03-03 17:10:49 +0530

    Room Route and models Updated

--- server/models/rooms.model.js
+++ server/models/rooms.model.js
@@ -2,16 +2,51 @@ const mongoose = require("mongoose");
 
 const roomsSchema = mongoose.Schema({
   roomid: { type: String, required: true },
-  room_capacity: { type: Number, require: true,default:4 },
+ 
   room_availability: { type: Boolean, default: true },
   players: [
     {
       user_id: { type: String, required: true },
+      username:{ type: String, required: true },
+      score:{ type: Number,default: 0}
     }
   ],
-  winer: {
+  level1:{
+    word_id:String,
+    question:String,
+    word:String,
+    hint:String,
+    clue1:String,
+    clue2:String
+  },
+  level2:{
+    word_id:String,
+    question:String,
+    word:String,
+    hint:String,
+    clue1:String,
+    clue2:String
+  },
+  level3:{
+    word_id:String,
+    question:String,
+    word:String,
+    hint:String,
+    clue1:String,
+    clue2:String
+  },
+  level4:{
+    word_id:String,
+    question:String,
+    word:String,
+    hint:String,
+    clue1:String,
+    clue2:String
+  },
+
+  winner: {
     user_id: String,
-    wining_score: Number,
+    winning_score: Number,
   },
   game_over: { type: Boolean, default: false },
   created_at: { type: Date, required: true },
