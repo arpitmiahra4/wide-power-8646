7bf1c7682d54a09aee2761f7be2d3a4e3365b217
HARSHAL-AP <apsundeharshal129@gmail.com>
2023-03-04 15:52:20 +0530

    User Signup Route Updated with Avtar key

--- server/controllers/user.routes.js
+++ server/controllers/user.routes.js
@@ -10,7 +10,19 @@ const UserModel = require("../models/user.model"); // importing UserModel from m
 
 const key = process.env.KEY; // importing the key value from .env
 const salt = process.env.SALT; // imporing the salt value from .env
-
+const avtararr=[
+  "https://png.pngtree.com/png-clipart/20221207/ourmid/pngtree-fashion-boy-avatar-png-image_6514592.png",
+  "https://png.pngtree.com/png-clipart/20221207/ourmid/pngtree-old-man-cartoon-png-image_6514608.png",
+  "https://png.pngtree.com/png-clipart/20221207/ourmid/pngtree-blonde-avatar-png-image_6514610.png",
+  "https://png.pngtree.com/png-clipart/20221207/ourmid/pngtree-3d-boy-head-portrait-png-image_6514617.png",
+  "https://png.pngtree.com/png-clipart/20221207/ourmid/pngtree-purple-hair-girl-avatar-png-image_6514621.png",
+  "https://png.pngtree.com/png-clipart/20221207/ourmid/pngtree-girl-with-headphones-png-image_6514627.png",
+  "https://png.pngtree.com/png-clipart/20221207/ourmid/pngtree-rock-girl-avatar-png-image_6514639.png",
+  "https://png.pngtree.com/png-clipart/20221207/ourmid/pngtree-business-man-avatar-png-image_6514640.png",
+  "https://png.pngtree.com/png-clipart/20221207/ourmid/pngtree-sports-boy-png-image_6514641.png",
+  "https://png.pngtree.com/png-clipart/20221207/ourmid/pngtree-girl-with-golden-curly-hair-png-image_6514652.png",
+  "https://png.pngtree.com/png-clipart/20221207/ourmid/pngtree-art-boy-avatar-png-image_6514653.png"
+];
 
 const userRouter = express.Router(); // creating the saperate router for the user routes.
 
@@ -28,6 +40,7 @@ userRouter.get('/', async (req, res) => {
 userRouter.post("/signup", async (req, res) => {
     console.log("in the signup");
     let { username, email, mobile, password, no_of_wins, no_of_looses, scores } = req.body;
+    let user_avtar=avtararr[Math.floor(Math.random() * avtararr.length)];
     try {
         bcrypt.hash(password, +salt, async (err, hash) => {
             try {
@@ -35,11 +48,12 @@ userRouter.post("/signup", async (req, res) => {
                     res.status(400).send("error in the bcrypt try part and error is :- ", err);
 
                 } else {
-                    let user = new UserModel({ username, email, mobile, no_of_wins, no_of_looses, scores, password: hash });
+                    let user = new UserModel({ username, email, mobile, no_of_wins, no_of_looses, scores, password: hash ,avtar:user_avtar});
                     await user.save();
                     res.status(200).send({ message: "signup successfully.." });
                 }
             } catch (error) {
+              console.log(error)
                 res.status(400).send({ message: "error in the bcrypt catch part" });
             }
         });
