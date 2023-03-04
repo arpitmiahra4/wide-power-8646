cd4a465ed8678c4f9bf45367f96cb0a64d8a80c2
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-03-04 13:49:45 +0530

    working: Home.tsx going on

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,15 @@
+import { Box, Flex, Image } from '@chakra-ui/react'
 import React from 'react'
+import Styles from '../styles/Home.module.css'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box borderRadius={10} className={Styles.Main_box}>
+      <Flex flexDirection={"column"} className={Styles.main_flex} borderRadius={10} w="7%" h="90vh" >
+       <Image src='https://i.ibb.co/J5PGkq3/Screenshot-2023-03-04-134136.png' h={"90vh"} borderRadius={10}/>
+      </Flex>
+    </Box>
   )
 }
 
