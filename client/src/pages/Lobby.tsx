5b5e3aaa5efe8a60faef77bc940d0a70a0f44eca
RajParmar03 <rjbusiness03@gmail.com>
2023-03-04 11:35:55 +0530

    updated the link for fetching the data from the backend.

--- client/src/pages/Lobby.tsx
+++ client/src/pages/Lobby.tsx
@@ -7,66 +7,45 @@ import axios, { AxiosResponse } from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const getUsers = async (id: String): Promise<PlayerDetails[]> => {
-    let response: AxiosResponse<PlayerDetails[]> = await axios.get(`http://localhost:8080/room/getplayerlist/${id}`);
-    return response.data;
+    let response: AxiosResponse<any> = await axios.get(`http://localhost:8080/room/singleroom/${id}`);
+    return response.data[0].players;
 }
 
 const Lobby = () => {
 
-    const [users, setUsers] = useState<PlayerDetails[]>([{
-        username: "rajparmar",
-        email: "rajparmar123@gmail.com",
-        mobile: 1234567890,
-        password: "rajparmar123",
-        no_of_wins: 0,
-        no_of_looses: 0,
-        scores: 0
-    }, {
-        username: "rajparmar",
-        email: "rajparmar123@gmail.com",
-        mobile: 1234567890,
-        password: "rajparmar123",
-        no_of_wins: 0,
-        no_of_looses: 0,
-        scores: 0
-    }, {
-        username: "rajparmar",
-        email: "rajparmar123@gmail.com",
-        mobile: 1234567890,
-        password: "rajparmar123",
-        no_of_wins: 0,
-        no_of_looses: 0,
-        scores: 0
-    }]);
-    const [time, setTime] = useState<number>(0);
+    const [users, setUsers] = useState<PlayerDetails[]>([]);
+    // const [time, setTime] = useState<number>(0);
     const intervalId = useRef<any>(null);
-   
+
 
     const navigate = useNavigate();
 
-    const id: string = "1";
+    const id: string = "27a302";
 
     useEffect(() => {
-        intervalId.current  = setInterval(() => {
-            // getUsers(id).then((res) => {
-            //     setUsers(res);
-            // });
-            setTime((prev : number) : number => {
-                return prev + 3
+        intervalId.current = setInterval(() => {
+            getUsers(id).then((res) => {
+                setUsers(res);
             });
+            // setTime((prev: number): number => {
+            //     return prev + 3
+            // });
         }, 3000);
+        return (() => clearInterval(intervalId.current));   
     }, []);
-    
+
 
     useEffect(() => {
-        if (time >= 30) {
+        if (users.length >= 4) {
             clearInterval(intervalId.current);
             navigate("/");
-        }
-    } , [time]);
+        }                         
+    }, [users]);    
+
+    console.log(users);
 
     return (
-        <Grid className={styles.gridBox} templateColumns='repeat(2, 1fr)' gap={6} >
+        <Grid className={styles.gridBox} templateColumns='repeat(2, 1fr)' gap={"30px"} >
             <GridItem className={styles.gridItem}>
                 {
                     !users[0] ?
@@ -74,16 +53,17 @@ const Lobby = () => {
                             <Spinner
                                 thickness='4px'
                                 speed='0.5s'
-                                emptyColor='gray.200'
+                                // emptyColor='gray.200'
                                 color='white.500'
                                 size='xl'
                             />
+                            <Text className={styles.text}>Waiting for the user...</Text>
                         </Box>
                         :
                         <VStack className={styles.mainStack}>
                             <Image className={styles.userImage} src={"/userProfile.png"} alt={users[0].username} />
                             <Text className={styles.text}>User Name : {users[0].username}</Text>
-                            <Text className={styles.text}>Scores : {users[0].scores}</Text>
+                            <Text className={styles.text}>Scores : {users[0].scores || 0}</Text>
                         </VStack>
                 }
             </GridItem>
@@ -94,16 +74,18 @@ const Lobby = () => {
                             <Spinner
                                 thickness='4px'
                                 speed='0.5s'
-                                emptyColor='gray.200'
+                                // emptyColor='gray.200'
                                 color='white.500'
                                 size='xl'
                             />
+                            <Text className={styles.text}>Waiting for the user...</Text>
                         </Box>
+
                         :
                         <VStack className={styles.mainStack}>
                             <Image className={styles.userImage} src={"/userProfile.png"} alt={users[1].username} />
                             <Text className={styles.text}>User Name : {users[1].username}</Text>
-                            <Text className={styles.text}>Scores : {users[1].scores}</Text>
+                            <Text className={styles.text}>Scores : {users[1].scores || 0}</Text>
                         </VStack>
                 }
             </GridItem>
@@ -114,16 +96,18 @@ const Lobby = () => {
                             <Spinner
                                 thickness='4px'
                                 speed='0.5s'
-                                emptyColor='gray.200'
+                                // emptyColor='gray.200'
                                 color='white.500'
                                 size='xl'
                             />
+                            <Text className={styles.text}>Waiting for the user...</Text>
                         </Box>
+
                         :
                         <VStack className={styles.mainStack}>
                             <Image className={styles.userImage} src={"/userProfile.png"} alt={users[2].username} />
                             <Text className={styles.text}>User Name : {users[2].username}</Text>
-                            <Text className={styles.text}>Scores : {users[2].scores}</Text>
+                            <Text className={styles.text}>Scores : {users[2].scores || 0}</Text>
                         </VStack>
                 }
             </GridItem>
@@ -134,7 +118,7 @@ const Lobby = () => {
                             <Spinner
                                 thickness='4px'
                                 speed='0.5s'
-                                emptyColor='gray.200'
+                                // emptyColor='gray.200'
                                 color='white.500'
                                 size='xl'
                             />
@@ -144,7 +128,7 @@ const Lobby = () => {
                         <VStack className={styles.mainStack}>
                             <Image className={styles.userImage} src={"/userProfile.png"} alt={users[3].username} />
                             <Text className={styles.text}>User Name : {users[3].username}</Text>
-                            <Text className={styles.text}>Scores : {users[3].scores}</Text>
+                            <Text className={styles.text}>Scores : {users[3].scores || 0}</Text>
                         </VStack>
                 }
             </GridItem>
